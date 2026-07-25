efbdee53ccbe10882ceb2edd761c82ad383dd944
prathameshpp-007 <007prathameshppatil@gmail.com>
2026-07-25 23:09:42 +0530

    Initial Commit

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+def is_palindrome(s):
+    s = s.lower().replace(" ", "")
+    return s == s[::-1]
+
+num = input("Enter a number or string: ")
+if is_palindrome(num):
+    print(f"'{num}' is a palindrome")
+else:
+    print(f"'{num}' is not a palindrome")
